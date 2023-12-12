a67ba6c5b706eed9d1d17f288d6f63c42df9fc47
corinachistol <chistolcorina19@gmail.com>
2023-12-12 11:02:52 +0200

    lifting the state up

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,27 +1,53 @@
 import {useState} from 'react';
 
 export default function Accordion({data}) {
+    const [currOpen,setCurrOpen] = useState(null)
     return (
         <div className="accordion">
             {data.map((el,idx)=>(
-                <AccordionItem number={idx+1} title={el.title} text={el.text} key={el.title}/>
+                <AccordionItem 
+                    currOpen={currOpen} 
+                    onOpen={setCurrOpen} 
+                    number={idx} 
+                    title={el.title} 
+                    key={el.title}
+                >
+                     {el.text}
+                </AccordionItem>
             ))}
+
+                <AccordionItem 
+                    currOpen={currOpen} 
+                    onOpen={setCurrOpen} 
+                    number={23} 
+                    title="Test 1" 
+                    key="test 1"
+                >
+                     <p>Allows React Developers to:</p>
+                     <ul>
+                        <li>Break up UI into components</li>
+                        <li>Make components reusable</li>
+                        <li>Place state efficiently</li>
+                     </ul>
+                </AccordionItem>
         </div>
     ) 
   }
 
-  function AccordionItem({number,title,text}){
-    const [isOpen,setIsOpen] = useState(false)
+
+  function AccordionItem({number,title,currOpen,onOpen, children}){
+    
+    const isOpen = number === currOpen ;
 
     function handleToggle() {
-        setIsOpen(isOpen => !isOpen)
+      onOpen(isOpen ? null : number)
     }
     return(
         <div className={`item ${isOpen ? 'open' : ''} `} onClick={handleToggle}>
             <p className="number">{number < 9 ? `0${number+1}` : number+1 }</p>
             <p className="title">{title}</p>
             <p className="icon">{isOpen ? '-' : '+'}</p>
-            {isOpen && <div className="content-box">{text}</div>}
+            {isOpen && <div className="content-box">{children}</div>}
             
         </div>
     )
